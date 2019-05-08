d7638f224ce4129a6b54ac89e718e633f18aeaf3
Reema <it.reema7@gmail.com>
2019-05-08 17:51:57 +0300

    Add files via upload

---
+++ Names to Faces/Names to Faces/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  Names to Faces
+//
+//  Created by mac_os on 03/09/1440 AH.
+//  Copyright © 1440 mac_os. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}
---
+++ Names to Faces/Names to Faces/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Names to Faces
+//
+//  Created by mac_os on 03/09/1440 AH.
+//  Copyright © 1440 mac_os. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+  
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}
